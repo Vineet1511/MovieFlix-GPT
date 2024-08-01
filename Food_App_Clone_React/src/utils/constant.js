a8271f1f465e695624a7462acfa3308eeb245e10
Vineet Sonkar <142674189+Vineet1511@users.noreply.github.com>
2024-08-01 17:41:58 +0530

    Delete Food_App_Clone_React directory

--- Food_App_Clone_React/src/utils/constant.js
+++
@@ -1,3 +0,0 @@
-export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
-
-export const MENU_URL = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=23.210678032866454&lng=80.00120222568512&restaurantId="
